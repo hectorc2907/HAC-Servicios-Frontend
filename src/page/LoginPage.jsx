613656486e19051384d20332e5376eeb7fc08717
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-16 13:43:37 -0300

    configuramos un modelo basico del LoginPage.jsx

--- src/page/LoginPage.jsx
+++ src/page/LoginPage.jsx
@@ -1,7 +1,60 @@
+import { useForm } from "react-hook-form";
+import { useAuth } from "../context/AuthContext";
+
 function LoginPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const { errors: loginErrors } = useAuth();
+
+  const onSubmit = handleSubmit(async (values) => {
+    console.log(values);
+  });
   return (
-    <div>LoginPage</div>
-  )
+    <div className="flex h-[calc(100vh-100px)] items-center justify-center">
+      <div className="bg-slate-100 max-w-md w-full p-10 rounded-md">
+        <h1 className="text-2xl font-bold my-2 text-center">Login</h1>
+        <form onSubmit={onSubmit}>
+          <input
+            type="email"
+            className="w-full px-4 py-2 rounded-lg my-2"
+            {...register("email", { required: true })}
+            placeholder="Email"
+          />
+          {errors.email && (
+            <p className="text-red-500">El Email es obligatorio</p>
+          )}
+          <input
+            type="password"
+            className="w-full px-4 py-2 rounded-lg my-2"
+            {...register("password", { required: true })}
+            placeholder="Password"
+          />
+          {errors.password && (
+            <p className="text-red-500">El Passowrd es obligatorio</p>
+          )}
+          <div className="flex justify-center">
+            <button
+              type="submit"
+              className="bg-green-500 text-white px-4 py-2 rounded-md my-2"
+            >
+              Iniciar Sesion
+            </button>
+          </div>
+        </form>
+        {loginErrors.map((errors, i) => (
+          <div
+            className="bg-red-500 p-2 text-white text-center my-2 rounded-md"
+            key={i}
+          >
+            {errors}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default LoginPage
+export default LoginPage;
