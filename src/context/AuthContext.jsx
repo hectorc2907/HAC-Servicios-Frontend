31aec16076df5df77d2061d787a9e42d6b543d9f
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-16 12:37:49 -0300

    configuramos la funcion del registro mediante signup en el contexto Auth

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,4 +1,5 @@
 import { createContext, useContext } from "react";
+import { registerRequest } from "../api/auth";
 
 export const AuthContext = createContext();
 
@@ -7,4 +8,20 @@ export const useAuth = () => {
   if (!context) {
     throw new Error("use Auth must be used within an AuthProvider");
   }
+  return context;
+};
+
+export const AuthProvider = ({ children }) => {
+  const signup = async (user) => {
+    try {
+      const res = await registerRequest(user);
+      console.log(res.data);
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
+
+  return (
+    <AuthContext.Provider value={{ signup }}>{children}</AuthContext.Provider>
+  );
 };
