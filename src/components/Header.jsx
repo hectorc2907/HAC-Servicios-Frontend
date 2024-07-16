bb6def4c190b42c8ce4a2dd8f4c362231a3c93c7
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-16 12:03:40 -0300

    se guardan avances hasta el momento para la refactorizacion del codigo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,52 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
 import { logout } from "../api/auth";
+import { useEffect } from "react";
 
 export function Header() {
+  const navigate = useNavigate();
   const { isAuthenticated, setIsAuthenticated } = useAuth();
+
+  useEffect(() => {
+    // Función para verificar la autenticación al cargar el componente
+    const checkAuthentication = () => {
+      // Obtener el valor de la cookie "access_token"
+      const token = getCookie("access_token");
+
+      if (token) {
+        setIsAuthenticated(true);
+      } else {
+        setIsAuthenticated(false);
+      }
+    };
+
+    checkAuthentication(); // Llamar a la función al cargar el componente
+  }, []);
+
+  const getCookie = (name) => {
+    const cookieValue = document.cookie.match(
+      "(^|;)\\s*" + name + "\\s*=\\s*([^;]+)"
+    );
+    return cookieValue ? cookieValue.pop() : "";
+  };
 
   const handleLogout = async () => {
     try {
       await logout();
       setIsAuthenticated(false);
+      navigate("/login");
     } catch (error) {
       console.error("Logout error:", error);
     }
   };
   return (
-    <header className="flex justify-between px-10 py-5">
+    <header className="flex justify-between items-center px-10 py-5">
       <div>
         <Link to="/">HAC Servicios</Link>
       </div>
       {isAuthenticated ? (
         <div className="grid grid-cols-1">
-          <button to="/logout" onClick={handleLogout} className="text-blue-600">
+          <button onClick={handleLogout} className="text-blue-600">
             Logout
           </button>
         </div>
