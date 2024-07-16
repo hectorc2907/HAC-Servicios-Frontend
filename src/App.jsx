1bc87516675b992b209d931b71494c115b690a2f
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-16 12:39:51 -0300

    configuramos en el AuthProvider en nuestro App.jsx para poder utilizar la funcion de registro

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
 import HomePage from "./page/HomePage";
+import RegisterPage from "./page/RegisterPage";
 
 export function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
