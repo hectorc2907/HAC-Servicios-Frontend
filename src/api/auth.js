229fbe0c5d2b909d829097f322189548efcd331a
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-16 12:14:43 -0300

    configuramos el auth.js con la ruta axios post al registro

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,27 +1,3 @@
-import axios from "axios";
+import axios from "./axios";
 
-const backend = import.meta.env.VITE_API_BACKEND;
-
-export const login = async (data) => {
-  try {
-    const response = await axios.post(`${backend}/login`, data, {
-      withCredentials: true,
-    });
-    console.log("Login Response:", response.data);
-    return response.data;
-  } catch (error) {
-    throw error.response?.data || error.message;
-  }
-};
-
-export const logout = async () => {
-  try {
-    const response = await axios.post(`${backend}/logout`, null, {
-      withCredentials: true,
-    });
-    console.log("Logout Response:", response.data);
-    return response.data;
-  } catch (error) {
-    throw error.response?.data || error.message;
-  }
-};
+export const registerRequest = (user) => axios.post(`/register`, user);
