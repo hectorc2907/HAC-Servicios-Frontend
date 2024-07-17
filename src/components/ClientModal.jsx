feb5378878a7c74a39a342a7f1231c393994971e
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-17 12:09:20 -0300

    estilamos el modal para que sea mas atractivo visualmente con sus botones y formulario

--- src/components/ClientModal.jsx
+++ src/components/ClientModal.jsx
@@ -1,15 +1,49 @@
-function ClientModal({ isOpen, onClose, children }) {
+function ClientModal({ isOpen, onClose }) {
   if (!isOpen) return null;
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-gray-500 bg-opacity-50">
-      <div className="bg-white p-6 rounded-lg shadow-lg">
-        Hola Mundo
-        <button
-          onClick={onClose}
-          className="mt-4 bg-red-500 text-white p-2 rounded"
-        >
-          Cerrar
-        </button>
+    <div className="px-2 fixed inset-0 flex items-center justify-center bg-gray-500 bg-opacity-50">
+      <div className="bg-slate-100 p-6 rounded-lg shadow-lg">
+        <h1 className="text-center text-2xl mb-2">Cliente Nuevo</h1>
+        <form>
+          <label htmlFor="firstName">Nombre</label>
+          <input
+            type="text"
+            className="w-full px-4 py-2 rounded-lg my-2"
+            placeholder="Nombre"
+          />
+          <label htmlFor="lastName">Apellido</label>
+          <input
+            type="text"
+            className="w-full px-4 py-2 rounded-lg my-2"
+            placeholder="Apellido"
+          />
+          <label htmlFor="phoneNumber">Telefono</label>
+          <input
+            type="number"
+            className="w-full px-4 py-2 rounded-lg my-2"
+            placeholder="Telefono"
+          />
+          <label htmlFor="address">Direccion</label>
+          <input
+            type="text"
+            className="w-full px-4 py-2 rounded-lg my-2"
+            placeholder="Direccion"
+          />
+          <div className="flex gap-x-2 justify-end">
+            <button
+              type="submit"
+              className="mt-4 bg-green-500 text-white p-2 rounded"
+            >
+              Guardar
+            </button>
+            <button
+              onClick={onClose}
+              className="mt-4 bg-red-500 text-white p-2 rounded"
+            >
+              Cerrar
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
