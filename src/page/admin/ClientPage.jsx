c3fd9009c554a270d756d8a849fcc892e50b540f
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-17 11:44:06 -0300

    se agrega un titulo y un boton para agregar clientes en el clientPage

--- src/page/admin/ClientPage.jsx
+++ src/page/admin/ClientPage.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useService } from "../../context/ServiceContext";
 import ClientCard from "../../components/ClientCard";
+import { FiPlusCircle } from "react-icons/fi";
 
 function ClientPage() {
   const { client, getClients } = useService();
@@ -13,6 +14,13 @@ function ClientPage() {
   if (client.length === 0) return <h1>No hay clientes</h1>;
   return (
     <div>
+      <h1 className="text-center text-3xl">Clientes</h1>
+      <div className="flex justify-center mt-3 mb-2">
+        <button className="bg-slate-200 rounded-lg p-4 flex flex-col items-center">
+          <FiPlusCircle className="text-3xl" />
+          <p>Agregar</p>
+        </button>
+      </div>
       {client.map((clients) => (
         <ClientCard clients={clients} key={client._id} />
       ))}
