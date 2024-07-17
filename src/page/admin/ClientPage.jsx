e9bae718b869eb8ab5a02d655a75e2da4c00ae39
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-17 10:59:39 -0300

    implementamos el getClient para recuperar los datos en su componente funcionando correctamente

--- src/page/admin/ClientPage.jsx
+++ src/page/admin/ClientPage.jsx
@@ -1,7 +1,22 @@
+import { useEffect } from "react";
+import { useService } from "../../context/ServiceContext";
+
 function ClientPage() {
+  const { client, getClients } = useService();
+
+  useEffect(() => {
+    getClients();
+    console.log(client)
+  }, []);
+
+  if (client.length === 0) return <h1>No hay clientes</h1>;
   return (
-    <div>ClientPage</div>
-  )
+    <div>
+      {client.map((clients) => (
+        <li key={clients._id}>{clients.firstName}</li>
+      ))}
+    </div>
+  );
 }
 
-export default ClientPage
+export default ClientPage;
