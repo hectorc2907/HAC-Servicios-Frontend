fd2b0a0fe1dccdd56727589b2627a7cb917724c4
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-18 15:35:55 -0300

    estilamos mejor la caja y agregamos los botones de uso

--- src/components/TripCard.jsx
+++ src/components/TripCard.jsx
@@ -1,16 +1,39 @@
-function TripCard({trip}) {
+import { MdDelete } from "react-icons/md";
+import { CgMoreR } from "react-icons/cg";
+import { useService } from "../context/ServiceContext";
+
+function TripCard({ trip }) {
+  const {} = useService();
+
+  const handleDelete = async (id) => {};
+
   return (
     <div className="bg-slate-200 my-2 rounded-lg">
       <div className="bg-slate-300 rounded-t-lg">
-        <div className="grid grid-col-2 p-2">
-          <h3>Ingresos:{trip.income}</h3>
-          <h3>Egresos:{trip.expenses}</h3>
-          <h3>Balance:{trip.balance}</h3>
+        <div className="flex flex-col px-2 py-4 gap-y-2">
+          <div className="grid grid-cols-2">
+            <h3>Ingresos:{trip.income}</h3>
+            <h3>Egresos:{trip.expenses}</h3>
+            <h3>Balance:{trip.balance}</h3>
+          </div>
+          <div className="flex flex-col">
+            <p>Creado:{trip.createdAt}</p>
+            <p>Actualizacion:{trip.updatedAt}</p>
+          </div>
         </div>
       </div>
       <div className="flex justify-around px-4 py-6">
-        <p>Creado:{trip.createdAt}</p>
-        <p>Actualizacion:{trip.updatedAt}</p>
+        <button className="flex flex-col items-center">
+          <CgMoreR className="text-2xl" />
+          <p>Detalles</p>
+        </button>
+        <button
+          className="flex flex-col items-center"
+          onClick={() => handleDelete(trip._id)}
+        >
+          <MdDelete className="text-2xl" />
+          <p>Borrar</p>
+        </button>
       </div>
     </div>
   );
