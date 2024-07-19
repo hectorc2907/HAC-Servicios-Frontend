36ca3c2a902fc7f7efc312313480834d669e536e
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-19 14:15:44 -0300

    estilamos la seccion de ventas principales

--- src/page/admin/SalePage.jsx
+++ src/page/admin/SalePage.jsx
@@ -1,7 +1,32 @@
+import { useService } from "../../context/ServiceContext";
+import { MoonLoader } from "react-spinners";
+import { FiPlusCircle } from "react-icons/fi";
+
 function SalePage() {
+  const { loading } = useService();
   return (
-    <div>SalePage</div>
-  )
+    <div>
+      <h1 className="text-center text-3xl">Detalles</h1>
+      <div className="flex justify-center mt-3 mb-2">
+        <form>
+          <button
+            type="submit"
+            className="bg-slate-200 rounded-lg p-4 flex flex-col items-center"
+          >
+            <FiPlusCircle className="text-3xl" />
+            <p>Agregar</p>
+          </button>
+        </form>
+      </div>
+      {loading ? (
+        <div className="flex h-[calc(100vh-400px)] items-center justify-center">
+          <MoonLoader color="#0d16fc" />
+        </div>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
 }
 
-export default SalePage
+export default SalePage;
