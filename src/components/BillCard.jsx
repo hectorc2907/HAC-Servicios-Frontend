d6e53d6d26f3252859769a268cbc9e9ab1a6cca3
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-22 10:56:15 -0300

    agregamos este mismo mensaje tanto a la tarjeta de gastos como a la de clientes

--- src/components/BillCard.jsx
+++ src/components/BillCard.jsx
@@ -6,42 +6,56 @@ function BillCard({ bill, onUpdate }) {
   const { getBills, deleteBill } = useService();
 
   const handleDelete = async (id) => {
-    await deleteBill(id);
-    await getBills();
-  };
-  return (
-    <>
-      <div className="bg-slate-100 my-2 rounded-md">
-        <div className={`bg-blue-500 text-white rounded-t-lg py-2`}>
-          <div className="grid grid-cols-1">
-            <p className="ps-3">Tipo de Gasto: {bill.types}</p>
-            <p className="ps-3">Valor: ${bill.value}</p>
-            {bill.description === "" ? (
-              ""
-            ) : (
-              <p className="ps-3">Detalles: {bill.description}</p>
-            )}
+    Swal.fire({
+      title: "¿Estás seguro de que deseas borrar el gasto?",
+      text: "¡No podrás revertir esto!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#E74C3C",
+      cancelButtonColor: "#2ECC71",
+      confirmButtonText: "Borrar",
+      cancelButtonText: "Cancelar",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        await deleteBill(id);
+        await getBills();
+        Swal.fire("¡Borrado!", "El gasto ha sido borrado.", "success");
+      }
+    });
+    return (
+      <>
+        <div className="bg-slate-100 my-2 rounded-md">
+          <div className={`bg-blue-500 text-white rounded-t-lg py-2`}>
+            <div className="grid grid-cols-1">
+              <p className="ps-3">Tipo de Gasto: {bill.types}</p>
+              <p className="ps-3">Valor: ${bill.value}</p>
+              {bill.description === "" ? (
+                ""
+              ) : (
+                <p className="ps-3">Detalles: {bill.description}</p>
+              )}
+            </div>
+          </div>
+          <div className="flex justify-evenly py-4">
+            <button
+              className="flex flex-col items-center"
+              onClick={() => onUpdate(bill)}
+            >
+              <GrDocumentUpdate className="text-2xl" />
+              <p>Actualizar</p>
+            </button>
+            <button
+              className="flex flex-col items-center"
+              onClick={() => handleDelete(bill._id)}
+            >
+              <RiDeleteBin6Line className="text-2xl" />
+              <p>Borrar</p>
+            </button>
           </div>
         </div>
-        <div className="flex justify-evenly py-4">
-          <button
-            className="flex flex-col items-center"
-            onClick={() => onUpdate(bill)}
-          >
-            <GrDocumentUpdate className="text-2xl" />
-            <p>Actualizar</p>
-          </button>
-          <button
-            className="flex flex-col items-center"
-            onClick={() => handleDelete(bill._id)}
-          >
-            <RiDeleteBin6Line className="text-2xl" />
-            <p>Borrar</p>
-          </button>
-        </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  };
 }
 
 export default BillCard;
--- src/components/ClientCard.jsx
+++ src/components/ClientCard.jsx
@@ -8,52 +8,66 @@ function ClientCard({ client, onUpdate }) {
   const { getClients, deleteClient } = useService();
 
   const handleDelete = async (id) => {
-    await deleteClient(id);
-    await getClients();
-  };
-  return (
-    <div className="bg-slate-100 my-2 rounded-lg">
-      <div className="text-white bg-blue-500 rounded-t-lg py-2">
-        <div className="grid grid-cols-1 px-5">
-          <p>Nombre: {client.firstName}</p>
-          <p>Apodo: {client.nickName}</p>
-          <p>Direccion: {client.address}</p>
+    Swal.fire({
+      title: "¿Estás seguro de que deseas borrar el cliente?",
+      text: "¡No podrás revertir esto!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#E74C3C",
+      cancelButtonColor: "#2ECC71",
+      confirmButtonText: "Borrar",
+      cancelButtonText: "Cancelar",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        await deleteClient(id);
+        await getClients();
+        Swal.fire("¡Borrado!", "El cliente ha sido borrado.", "success");
+      }
+    });
+    return (
+      <div className="bg-slate-100 my-2 rounded-lg">
+        <div className="text-white bg-blue-500 rounded-t-lg py-2">
+          <div className="grid grid-cols-1 px-5">
+            <p>Nombre: {client.firstName}</p>
+            <p>Apodo: {client.nickName}</p>
+            <p>Direccion: {client.address}</p>
+          </div>
+        </div>
+        <div className="flex justify-evenly py-4">
+          <a
+            href={`tel:${client.phoneNumber}`}
+            target="_blank"
+            className="flex flex-col items-center"
+          >
+            <MdPhoneForwarded className="text-2xl" />
+            LLamar
+          </a>
+          <a
+            href={`https://wa.me/${client.phoneNumber}`}
+            target="_blank"
+            className="flex flex-col items-center"
+          >
+            <FaWhatsapp className="text-2xl" />
+            Whatsapp
+          </a>
+          <button
+            className="flex flex-col items-center"
+            onClick={() => onUpdate(client)}
+          >
+            <GrDocumentUpdate className="text-2xl" />
+            <p>Actualizar</p>
+          </button>
+          <button
+            className="flex flex-col items-center"
+            onClick={() => handleDelete(client._id)}
+          >
+            <RiDeleteBin6Line className="text-2xl" />
+            <p>Borrar</p>
+          </button>
         </div>
       </div>
-      <div className="flex justify-evenly py-4">
-        <a
-          href={`tel:${client.phoneNumber}`}
-          target="_blank"
-          className="flex flex-col items-center"
-        >
-          <MdPhoneForwarded className="text-2xl" />
-          LLamar
-        </a>
-        <a
-          href={`https://wa.me/${client.phoneNumber}`}
-          target="_blank"
-          className="flex flex-col items-center"
-        >
-          <FaWhatsapp className="text-2xl" />
-          Whatsapp
-        </a>
-        <button
-          className="flex flex-col items-center"
-          onClick={() => onUpdate(client)}
-        >
-          <GrDocumentUpdate className="text-2xl" />
-          <p>Actualizar</p>
-        </button>
-        <button
-          className="flex flex-col items-center"
-          onClick={() => handleDelete(client._id)}
-        >
-          <RiDeleteBin6Line className="text-2xl" />
-          <p>Borrar</p>
-        </button>
-      </div>
-    </div>
-  );
+    );
+  };
 }
 
 export default ClientCard;
