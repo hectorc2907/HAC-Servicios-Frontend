ef9b36bba6ea5f97b0a21b67f4e5c36b64b0407d
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-22 13:42:32 -0300

    configuramos un mensaje principal al sistema

--- src/page/HomePage.jsx
+++ src/page/HomePage.jsx
@@ -1,7 +1,10 @@
 function HomePage() {
   return (
-    <div>HomePage</div>
-  )
+    <div>
+      <h1 className="text-center text-2xl">HAC Servicios</h1>
+      <p className="text-center">Sistema de administracion comercial</p>
+    </div>
+  );
 }
 
-export default HomePage
+export default HomePage;
